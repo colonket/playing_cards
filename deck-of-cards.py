652b6a2974890dbb38ea6c20c0e968521da62213
Eric Iniguez <colonket@iniguez.dev>
2022-10-24 08:54:35 -0500

    rewrote implementation

--- deck-of-cards.py
+++ deck-of-cards.py
@@ -1,66 +1,37 @@
+#!/usr/bin/env python3
 import random
 
-# Python implementation of a deck of 52 cards.
-# Allows for deck shuffling and drawing 1 or more cards
-
 class Card:
-    def __init__(self, suit, rank):
+    def __init__(self,suit,rank):
         self.suit = suit
         self.rank = rank
 
-    # Returns the card name, for example "Ace of Spades"
-    def show(self):
-        nameSub = {
-            1:"Ace",
-            11:"Jack",
-            12:"Queen",
-            13:"King"
-        }
-        if self.rank in nameSub:
-            self.rank = nameSub[self.rank]
-        return (f"{self.rank} of {self.suit}")
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
 
     def __repr__(self):
-        return self.show()
+        return str((self.suit,self.rank))
 
-    def __str__(self):
-        return self.show()
-
-class Deck:
+class DeckOfCards:
     def __init__(self):
-        self.cards = []
+        self.suits = ["Spades","Diamonds","Hearts","Clubs"] 
+        self.ranks = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
+        self.cards = [ Card(s,r) for s in self.suits for r in self.ranks ]
 
-    # Creates a fresh 52 card deck
-    def build(self):
-        suits = ["Spades","Clubs","Hearts","Diamonds"]
-        ranks = range(1,13+1)
+    def shuffle(self, times=1):
+        for _ in range(times):
+            random.shuffle(self.cards)
 
-        self.cards = []
-        for s in suits:
-            for r in ranks:
-                self.cards.append(Card(s,r))
+    def deal(self, num):
+        hand = []
+        while num > 0:
+            hand += [self.cards.pop()]
+            num -= 1
+        return hand
 
-    # Print each card in the deck
-    def show(self):
-        for c in self.cards:
-            c.show()
+deck = DeckOfCards()
+print(len(deck.cards))
+deck.shuffle()
+print(deck.deal(7))
+print(len(deck.cards))
 
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw(self,quantity=1):
-        if quantity == 1:
-            return self.cards.pop()
-        else:
-            return [ self.cards.pop() for i in range(quantity) ]
-
-if __name__ == '__main__':
-    # Build a deck of 52 cards
-    deck = Deck()
-    deck.build()
-
-    # Shuffle the deck
-    deck.shuffle()
-
-    # Draw the top 7 cards
-    print(deck.draw(7))
